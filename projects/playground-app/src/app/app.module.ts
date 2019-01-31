fbd7943933f9aa1e66e5486740df866937dc5965
ManfredSteyer <manfred.steyer@gmx.net>
2019-01-31 17:46:13 +0100

    differential serving

--- projects/playground-app/src/app/app.module.ts
+++ projects/playground-app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler/src/core';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
     BrowserModule
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
